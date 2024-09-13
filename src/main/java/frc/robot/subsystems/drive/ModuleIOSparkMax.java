a4ad179c47c16573530930d3cf75a995f4f31de3
barbute <jackhdemeterio@gmail.com>
2024-09-13 12:56:40 -0500

    Configure REV hardware in ModuleIOSparkMax.java

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSparkMax.java
@@ -4,7 +4,9 @@
 
 package frc.robot.subsystems.drive;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
@@ -34,5 +36,43 @@ public class ModuleIOSparkMax implements ModuleIO {
     }
 
     INVERT_AZIMUTH_MOTOR = configuration.INVERT_AZIMUTH_MOTOR();
+
+    // Clear current settings of the motors, we want to control everything in code
+    DRIVE_MOTOR.restoreFactoryDefaults();
+    AZIMUTH_MOTOR.restoreFactoryDefaults();
+
+    DRIVE_MOTOR.setCANTimeout(DriveConstants.SPARK_CONFIGURATIONS.DRIVE_CAN_TIMEOUT_MS());
+    AZIMUTH_MOTOR.setCANTimeout(DriveConstants.SPARK_CONFIGURATIONS.AZIMUTH_CAN_TIMEOUT_MS());
+
+    DRIVE_MOTOR.setInverted(configuration.INVERT_DRIVE_MOTOR());
+    AZIMUTH_MOTOR.setInverted(INVERT_AZIMUTH_MOTOR);
+
+    DRIVE_MOTOR.setSmartCurrentLimit(
+        DriveConstants.SPARK_CONFIGURATIONS.DRIVE_SMART_CURRENT_LIMIT_AMP());
+    AZIMUTH_MOTOR.setSmartCurrentLimit(
+        DriveConstants.SPARK_CONFIGURATIONS.AZIMUTH_SMART_CURRENT_LIMIT_AMP());
+    DRIVE_MOTOR.enableVoltageCompensation(DriveConstants.SPARK_CONFIGURATIONS.NOMINAL_VOLTAGE());
+    AZIMUTH_MOTOR.enableVoltageCompensation(DriveConstants.SPARK_CONFIGURATIONS.NOMINAL_VOLTAGE());
+
+    DRIVE_ENCODER.setPosition(0.0);
+    DRIVE_ENCODER.setMeasurementPeriod(
+        DriveConstants.SPARK_CONFIGURATIONS.DRIVE_ENCODER_MEASUREMENT_PERIOD_MS());
+    DRIVE_ENCODER.setAverageDepth(2);
+
+    AZIMUTH_ENCODER.setPosition(0.0);
+    AZIMUTH_ENCODER.setMeasurementPeriod(
+        DriveConstants.SPARK_CONFIGURATIONS.AZIMUTH_ENCODER_MEASUREMENT_PERIOD_MS());
+    AZIMUTH_ENCODER.setAverageDepth(2);
+
+    DRIVE_MOTOR.setIdleMode(IdleMode.kBrake);
+    AZIMUTH_MOTOR.setIdleMode(IdleMode.kCoast);
+
+    DRIVE_MOTOR.setCANTimeout(0);
+    AZIMUTH_MOTOR.setCANTimeout(0);
+
+    DRIVE_MOTOR.burnFlash();
+    AZIMUTH_MOTOR.burnFlash();
+
+    CANCODER.getConfigurator().apply(new CANcoderConfiguration());
   }
 }
