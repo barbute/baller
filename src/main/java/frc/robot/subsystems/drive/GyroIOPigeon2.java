91a1e5789a2bbb4fb600aab14851334529b71b49
barbute <jackhdemeterio@gmail.com>
2024-09-19 13:07:04 -0500

    Instantiate gyro hardware
    Update gyro logged inputs

--- src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
@@ -4,7 +4,35 @@
 
 package frc.robot.subsystems.drive;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+
 /** Hardware implementation of a CTRE Pigeon 2.0 */
 public class GyroIOPigeon2 implements GyroIO {
-  public GyroIOPigeon2() {}
+  private final Pigeon2 pigeon = new Pigeon2(DriveConstants.DRIVE_CONFIGURATION.GYRO_ID());
+
+  private final StatusSignal<Double> yaw = pigeon.getYaw();
+  private final StatusSignal<Double> yawVelocity = pigeon.getAngularVelocityZWorld();
+
+  public GyroIOPigeon2() {
+    pigeon.getConfigurator().apply(new Pigeon2Configuration());
+    pigeon.getConfigurator().setYaw(0.0);
+
+    yaw.setUpdateFrequency(100.0);
+    yawVelocity.setUpdateFrequency(100.0);
+
+    pigeon.optimizeBusUtilization();
+  }
+
+  @Override
+  public void updateInputs(GyroIOInputs inputs) {
+    inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK);
+    inputs.yawPosition = Rotation2d.fromDegrees(yaw.getValueAsDouble());
+    inputs.yawVelocityRadPerSec = Units.degreesToRadians(yawVelocity.getValueAsDouble());
+  }
 }
